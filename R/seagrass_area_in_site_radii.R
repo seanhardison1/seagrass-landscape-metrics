85c5925988ff8cbf7bb14e34be58b3d2acb849be
seanhardison1 <seanhardison@gmail.com>
2020-07-16 10:09:53 -0600

    Finding seagrass area through time near sampling sites

--- R/seagrass_area_in_site_radii.R
+++ R/seagrass_area_in_site_radii.R
@@ -13,51 +13,58 @@ load(here::here("data/sample_locs.rdata"))
 
 sg <- bind_rows(hi_sg, sb_sg)
 
-buffed_locs <- 
-  sample_locs %>% 
-  st_transform(.,st_crs(sg)) %>% 
-  st_as_sf() %>% 
-  st_buffer(.,dist = 1000) %>% 
-  st_as_sf() %>% 
-  ungroup()
 
-t <- buffed_locs %>% 
-  filter(year == 2012, season == "summer") %>% 
-  dplyr::select(insideSeagrass)
-
-
-ggplot() +
-  geom_sf(data = t)
-
-
-out <- NULL
-for (i in 2012:2018){
-  int <- sg %>% dplyr::filter(YEAR == i)
+sg_coverage <- NULL
+for (d in c(500, 1000,2000,3000)){
   
-  buffed_int <- 
-    buffed_locs %>% 
-    dplyr::filter(year == i, season == "summer") %>% 
-    st_intersection(.,int) %>% 
-    group_by(site) %>% 
-    summarise(do_union = F) %>% 
-    mutate(sg_area = st_area(., by_feature = F),
-           year = i) %>% 
-    ungroup()
-
-    assign("out", bind_rows(buffed_int, out))
-}  
-
-
-library(plotly)
-ggplot() +
-  geom_sf(data = int) +
-  geom_sf(data = t, color = "red") +
-  geom_sf(data = buffed_int, fill = "transparent", color = "blue") 
+  message(d)
+  Sys.sleep(0.25)
+  buffed_locs <- 
+    sample_locs %>% 
+    st_transform(.,st_crs(sg)) %>% 
+    st_as_sf() %>% 
+    st_buffer(.,dist = d) %>% 
+    st_as_sf() %>% 
+    ungroup() %>% 
+    mutate(circle_area = as.numeric(st_area(.)))
   
+  for (i in 2012:2018){
+    for (j in 1:length(unique(buffed_locs$site))){
+      for (k in c("fall","summer")){
+        message(paste(i, unique(buffed_locs$site)[j], k))
+        
+        int <- sg %>% dplyr::filter(YEAR == i)
+        
+        buffed_int <- 
+          buffed_locs %>% 
+          dplyr::filter(year == i, 
+                        site == unique(buffed_locs$site)[j],
+                        season == k) %>% 
+          st_intersection(.,int) %>% 
+          group_by(year, season, site, circle_area) %>% 
+          summarise() %>% 
+          mutate(sg_area = as.numeric(st_area(.)),
+                 sg_cover = sg_area/circle_area,
+                 radius = d) %>% 
+          ungroup() 
+        
+        assign("sg_coverage", bind_rows(buffed_int, sg_coverage)) 
+      }
+    }
+  }  
+}
 
-plot(buffed_int %>% dplyr::select(site))
-plot(int, add = T)
+seagrass_coverage <- 
+sg_coverage %>% 
+  st_set_geometry(NULL) %>% 
+  mutate(year = as.numeric(as.character(year)),
+         site = as.factor(site)) %>% 
+  group_by(radius) %>% 
+  dplyr::select(-circle_area, -sg_area) %>% 
+  pivot_wider(names_from = radius,
+              names_prefix = "sg_cover_",
+              values_from = sg_cover,
+              names_sep = "_", values_fill = 0) 
 
-ggplot() +
-  geom_sf(data = out) +
-  facet_wrap(.~year)
+
+save(seagrass_coverage, file = here::here("data/seagrass_cover.rdata"))
