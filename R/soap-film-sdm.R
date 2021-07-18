6428d9a6d1e9a2b236436836e5aa736322a8ebd0
seanhardison1 <seanhardison@gmail.com>
2021-07-18 17:05:56 -0400

    fixing prediction errors

--- R/soap-film-sdm.R
+++ R/soap-film-sdm.R
@@ -28,7 +28,7 @@ train_poly1 <- hi_sg %>%
   dplyr::summarise() %>% 
   st_as_sf() %>% 
   smoothr::fill_holes(threshold = 50000) %>% 
-  smoothr::drop_crumbs(threshold = 1e6)
+  smoothr::drop_crumbs(threshold = 1e6) 
 
 tp_2017 <- train_poly1 %>% filter(year == 2017)
 tp_2017$geometry <- tp_2017$geometry + hack
@@ -38,7 +38,6 @@ train_poly <-
   st_transform(tp_2017, st_crs(train_poly1)) %>% 
   bind_rows(.,train_poly1 %>% filter(year == 2018)) %>% 
   dplyr::select(-year)
-
 
 ggplot() +
   geom_sf(data = train_poly)
@@ -61,23 +60,27 @@ syn_full <-
   arrange(desc(year)) %>% 
   # dplyr::select(-year) %>% 
   dream::sfc_as_cols() %>% 
-  st_set_geometry(NULL)
+  st_set_geometry(NULL) %>% 
+  mutate(x = x/1000,
+         y = y/1000)
 
 syn_full1 %<>% 
   dream::sfc_as_cols() %>% 
-  st_set_geometry(NULL)
+  st_set_geometry(NULL)%>% 
+  mutate(x = x/1000,
+         y = y/1000)
 
-syn_dat <- syn_full %>% slice(1:65)
+syn_dat <- syn_full %>% slice(1:80)
 head(syn_dat)
-syn_dat_tprs <- syn_full1 %>% slice(1:65)
+syn_dat_tprs <- syn_full1 %>% slice(1:80)
 head(syn_dat_tprs)
 
 tss_test_so  <- syn_full %>% slice(66:80)
 tss_test_tprs  <- syn_full1 %>% slice(66:80)
-
-ggplot() +
-  geom_sf(data = train_poly) +
-  geom_point(data = syn_dat, aes(x = x, y = y)) 
+# 
+# ggplot() +
+#   geom_sf(data = train_poly) +
+#   geom_point(data = syn_dat, aes(x = x, y = y)) 
 
 # geom_point(data = syn_dat, aes(x= x, y = y, color = shoots))
 
@@ -90,15 +93,17 @@ ggplot() +
 
 # Use TPRS to model how shoot density changes spatially
 syn_dat_tprs %<>% mutate(year = factor(year))
-m1 <- gam(shoots ~ s(x, y, by = year), data = syn_dat_tprs, method = "REML")
+m1 <- gam(shoots ~ s(x, y, by = year), data = syn_dat_tprs, method = "REML",
+          family = tw(link = "log"))
 summary(m1)
 
-r <- raster(extent(hi_sg),
-            res = 50,
-            crs ="+proj=utm +zone=18 +datum=WGS84 +units=m +no_defs")
+train_poly1 %<>% mutate(geometry = geometry/1000)
+
+r <- raster(extent(train_poly1),
+            res = 50/1000)
 
 pdf1 <- fasterize(train_poly1 %>% filter(year == 2017), r) %>% 
-  as("SpatialPixels") %>% 
+  as("SpatialPixels") %>%
   as.data.frame() %>% 
   mutate(year = 2017)
 
@@ -108,11 +113,13 @@ pdf2 <- fasterize(train_poly1 %>% filter(year == 2018), r) %>%
   mutate(year = 2018)
 
 pdf <- bind_rows(pdf1, pdf2) %>% 
-  mutate(year = factor(year))
-
+  mutate(year = factor(year)) #%>% 
+  # mutate(x = x/1000,
+         # y = y/1000)
 
 ##predictions
-pdata <- pdf %>% mutate(shoots = predict(m1, newdata = pdf, type = "response"))
+pdata <- pdf %>% mutate(shoots = predict(m1, newdata = pdf, 
+                                         type = "response"))
 tmp <- pdata %>% 
   st_as_sf(coords = c("x","y"), 
            crs = st_crs(train_poly1)) %>% 
@@ -140,7 +147,9 @@ crds <-
   tibble(x = c(crds1[,1],crds2[,1]),
        y = c(crds1[,2],crds2[,2]),
        year = c(rep(2017, nrow(crds1)),
-                rep(2018, nrow(crds2))))
+                rep(2018, nrow(crds2)))) %>% 
+  mutate(x = x/1000,
+         y = y/1000)
 
 names(crds) <- c("x", "y", "year")
 bound <- split(crds, crds$year)
@@ -148,11 +157,10 @@ bound <-lapply(bound,`[`, c(1, 2))
 nr <- seq(1, 2)
 bound <-lapply(nr, function(n) as.list.data.frame(bound[[n]]))
 
-## bound is a list of 3 lists (because 1 large polygon and 2 loops), each including 3 elements (utmx, utmy and f)
-## an element f is set to a vector of zeros to set the boundary condition
-bound <- lapply(nr, function(n) bound[[n]] <- c(bound[[n]], 
-                                                list(f = rep(0, 
-                                                             length(bound[[n]]$x)))))
+# # set boundary of smooth to zero density - if not set, mgcv will estimate the boundary
+# bound <- lapply(nr, function(n) bound[[n]] <- c(bound[[n]],
+#                                                 list(f = rep(200,
+#                                                              length(bound[[n]]$x)))))
 
 # generate knots within the boundary
 grid_out <- NULL
@@ -176,47 +184,66 @@ for (i in 1:nrow(train_poly)){
 grid <- 
   st_coordinates(grid_out) %>% 
   as_tibble() %>% 
-  dplyr::rename(x = X, y = Y)
+  dplyr::rename(x = X, y = Y) %>% 
+  mutate(x = x/1000,
+         y = y/1000) %>% 
+  add_row(x = c(435.3, 437.7, 435.2, 437.82),
+          y = c(4141.1, 4143.864, 4141.3, 4143.6))
+
 
 ggplot() +
-  geom_sf(data = train_poly) +
+  geom_sf(data = train_poly %>% mutate(geometry = geometry/1000)) +
   # geom_point(data = syn_dat, aes(x = x, y = y)) +
   geom_point(data = grid,
              aes(x = x, y = y), alpha = 0.25)
 
 
 # fit the model
-m2 <- gam(shoots ~ s(x, y, 
-                     bs = "so", 
+m2 <- gam(shoots ~ s(year, bs = "re") + 
+                     s(x, y,
+                     bs = "so",
                      xt = list(bnd = bound)),
-          data = syn_dat, method = "REML", 
-          knots = grid,
-          na.action = na.omit)
+          data = syn_dat, method = "REML",
+          knots = grid)
 
-summary(m2)
+m3 <- gam(shoots ~  s(x, y,
+              bs = "so",
+              xt = list(bnd = bound)),
+          data = syn_dat, method = "REML",
+          knots = grid)
+
+
+summary(m3)
+AIC(m1, m2, m3)
+gratia::draw(m2)
+gratia::appraise(m3)
 
 lims <- apply(crds, 2, range)
 ylim <- lims[,2]
 xlim <- lims[,1]
 
-plot(m2, asp = 1, ylim = ylim, 
+plot(m3, asp = 1, ylim = ylim, 
      xlim = xlim, se = FALSE, scheme = 2, main = "")
 
 crds <- st_coordinates(train_poly)[,c("X","Y")]
-grid.x <- with(m1$var.summary,
-               seq(min(c(x, crds[,1])), max(c(x, crds[,1])), by = 50))
-grid.y <- with(m1$var.summary,
-               seq(min(c(y, crds[,2])), max(c(y, crds[,2])), by = 50))
-pdata_grid <- with(m2$var.summary, expand.grid(x = grid.x, y = grid.y))
-names(pdata_grid) <- c("x","y")
+crds[,1] <- crds[,1]/1000
+crds[,2] <- crds[,2]/1000
 
-pdata2 <- transform(pdata_grid[, 1:2], shoots = predict(m2, newdata = pdata_grid,
+grid.x <- with(m3$var.summary,
+               seq(min(c(x, crds[,1])), max(c(x, crds[,1])), by = 50/1000))
+grid.y <- with(m3$var.summary,
+               seq(min(c(y, crds[,2])), max(c(y, crds[,2])), by = 50/1000))
+pdata_grid <- with(m3$var.summary, expand.grid(x = grid.x, y = grid.y,
+                                               year = c(2017, 2018)))
+names(pdata_grid) <- c("x","y","year")
+
+pdata2 <- transform(pdata_grid, shoots = predict(m2, newdata = pdata_grid,
                                                         type = "response"))
 
 (soap <- 
   ggplot() +
   geom_raster(data = pdata2, aes(x = x, y = y, fill = shoots)) +
-  geom_sf(data = train_poly, fill = "transparent") +
+  geom_sf(data = train_poly %>% mutate(geometry = geometry/1000), fill = "transparent") +
     geom_contour(data = pdata2, aes(x = x, y = y, z = shoots),
                  show.legend = F, color = "grey70",
                  bins = 6) +
@@ -224,10 +251,10 @@ pdata2 <- transform(pdata_grid[, 1:2], shoots = predict(m2, newdata = pdata_grid
                       size = 2, label.placement = label_placement_n(1),
                       skip = 1, color = "grey10") +
   viridis::scale_fill_viridis(na.value = NA,
-                              limits = c(0,550)) +
+                              limits = c(0,600)) +
   theme_minimal() +
   theme(axis.title = element_blank(),
-        axis.text = element_text(size = 5),
+        axis.text = element_text(size = 10),
         legend.title = element_textbox()) +
   labs(fill = "Shoots m<sup>-2</sup>"))
 
@@ -243,8 +270,8 @@ pdata2 <- transform(pdata_grid[, 1:2], shoots = predict(m2, newdata = pdata_grid
                     skip = 1, color = "grey10") +
   facet_wrap(~year) +
   viridis::scale_fill_viridis(na.value = NA,
-                              limits = c(-250,600)) +
-  theme_minimal() +
+                              limits = c(0,800)) +
+  theme_bw() +
   theme(axis.title = element_blank(),
         axis.text = element_text(size = 5),
         legend.title = element_textbox()) +
@@ -256,20 +283,34 @@ soap + tprs + plot_layout(guides = "collect") & theme(legend.position = 'bottom'
 ggsave(filename = here::here("sdm_comparison.png"),
        dpi = 200, width = 7, height = 3)
 
-tprs_test_pred <- predict(m1,newdata=tss_test_tprs, type = "response")
-soap_test_pred <- predict(m2,newdata=tss_test_so, type = "response")
+
+(tprs_test_pred <- predict(m1,newdata=tss_test_tprs, type = "response"))
+(soap_test_pred <- predict(m2,newdata=tss_test_so, type = "response"))
 
 (oos_pred <- 
   AIC(m1, m2) %>% 
   mutate(RMSE = c(sqrt(mean((tss_test_tprs$shoots - tprs_test_pred)^2)),
                   sqrt(mean((tss_test_so$shoots - soap_test_pred)^2)))))
 
+tprs_test_pred2 <- predict(m1,newdata=syn_dat_tprs, type = "response")
+soap_test_pred2 <- predict(m3,newdata=syn_dat, type = "response")
+
+(is_pred <- 
+    AIC(m1, m3) %>% 
+    mutate(RMSE = c(sqrt(mean((syn_dat_tprs$shoots - tprs_test_pred2)^2)),
+                    sqrt(mean((syn_dat$shoots - soap_test_pred2)^2)))))
+
 row.names(oos_pred) <- c("Kriging","Soap-film")
 
-syn_full %>%
-  mutate(soap_test_pred = as.numeric(predict(m2, newdata = ., type = "response")),
-         tprs_test_pred = as.numeric(predict(m1, newdata = ., type = "response"))) %>% 
+sf_pred <- syn_dat %>% 
+  mutate(soap_test_pred = as.numeric(predict(m3, newdata = ., type = "response")))
+
+tprs_pred <- syn_dat_tprs %>% 
+  mutate(tprs_test_pred = as.numeric(predict(m1, newdata = ., type = "response")))
+
+bind_rows(sf_pred, tprs_pred) %>%
   gather(var, value, -x,-y,-shoots,-year) %>% 
   ggplot() +
     geom_point(aes(x = shoots, y = value)) +
-    facet_wrap(.~var)
+    facet_wrap(year~var, scales = "free") +
+    geom_abline(aes(intercept = 0, slope = 1))
